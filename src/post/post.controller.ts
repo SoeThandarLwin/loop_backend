548891a790fd3ea50e400caeb2700d23b54261a8
SoeThandarLwin <soethandar.lwin@mail.kmutt.ac.th>
2024-06-09 20:18:59 +0700

    implement request and dele post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -3,6 +3,7 @@ import { Post } from "./post.model";
 import { v4 as uuidv4 } from "uuid";
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from '../auth/auth_model';
+import useId from "@mui/material/utils/useId";
 
 
 export async function createPost(req: Request, res: Response) {
@@ -15,14 +16,20 @@ export async function createPost(req: Request, res: Response) {
   return res.status(201).json(returnedPost).send();
 }
 
-export function requestPost(req: Request, res: Response) {
-  res.send("request post");
-}
-
-export function updatePost(req: Request, res: Response) {
-  res.send("update post");
+export async function requestPost(req: Request, res: Response) {
+  const postId = req.query.post_id;
+  const findByQueryPost = await Post.findById(postId).exec();
+  if (!findByQueryPost) return res.status(404).send();
+  return res.json(findByQueryPost).send();
 }
 
 export function deletePost(req: Request, res: Response) {
-  res.send("delete post");
+  const getHeader = req.headers.authorization;
+  const token = getHeader?.split(' ') [1];
+  const user: string | JwtPayload = jwt.verify(token!, process.env.JWT_KEY as string) as typeof User;
+  if (!user) return res.json({message: 'invalid token'}).send();
+
+  const postId = req.query.post_id;
+  Post.deleteOne({ _id : postId, user: user.id })
+  return res.status(204).send();
 }
--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { createPost, requestPost, updatePost, deletePost } from "./post.controller";
+import { createPost, requestPost, deletePost } from "./post.controller";
 
 export const postRouter = Router();
 
 postRouter.post('/', createPost);
 postRouter.get('/', requestPost);
-postRouter.put('/', updatePost);
 postRouter.delete('/', deletePost);
