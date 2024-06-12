510542649313695fdc5e294dde02614a86a711f9
smyd <sumyat.yada@kmutt.ac.th>
2024-06-13 02:54:16 +0700

    main

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,6 @@ import { mediaRouter } from "./media/media.router";
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CONNECTION_URL}/Loop?retryWrites=true&w=majority&appName=Cluster0`;
 
-const connection = await mongoose.connect(uri);
-console.log('connection successful');
-
 const app: Express = express();
 const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
